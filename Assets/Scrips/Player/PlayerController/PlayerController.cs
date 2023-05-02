f7fe58eacf8c076d9eb81bdb74e454a1f2ad9fa3
Jonas Ingelstad <JonasIngelstad@Gmail.com>
2023-05-02 23:39:53 +0200

    Tweaked dash method

--- Assets/Scrips/Player/PlayerController/PlayerController.cs
+++ Assets/Scrips/Player/PlayerController/PlayerController.cs
@@ -7,10 +7,15 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private Rigidbody _rb;
     [SerializeField] private float _speed = 5;
     [SerializeField] private float _turnSpeed = 360;
-    [SerializeField] private float _dash = 500;
+    [SerializeField] private float _dashDistance = 1;
+    [SerializeField] private float _dashSpeed = 1;
+
+    private bool _doDash;
 
     public Transform _dashSparkle;
     private ParticleSystem.EmissionModule _dashSparkleParticles;
+
+    private Vector3 _dashPos;
 
     private Vector3 _input;
 
@@ -59,22 +64,27 @@ public class PlayerController : MonoBehaviour
     {
         if (Input.GetKeyDown("space"))
         {
-            _rb.AddForce(transform.forward * _dash);
-
-            _dashSparkleParticles.enabled = true;
-            StartCoroutine(stopDashSparkles());
- 
+            _dashPos = transform.position + transform.forward * _dashDistance;
+            _doDash = true;
+            StartCoroutine(DoSparkle());
             Debug.Log("dash");
+        }
+
+        if (_doDash)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, _dashPos, _dashSpeed * Time.deltaTime);
+
+            if (Vector3.Distance(transform.position, _dashPos) < 0.001f)
+                _doDash = false;
         }
     }
 
-    IEnumerator stopDashSparkles()
+    IEnumerator DoSparkle()
     {
+        _dashSparkleParticles.enabled = true;
         yield return new WaitForSeconds(.1f);
         _dashSparkleParticles.enabled = false;
     }
-
-   
 }
 
 public static class Helpers
