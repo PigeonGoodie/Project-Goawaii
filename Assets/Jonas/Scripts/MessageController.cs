93b4c6b8518f6419273796281bd61b27f7a883d0
Jonas Ingelstad <JonasIngelstad@Gmail.com>
2023-05-18 11:15:13 +0200

    Test message system

---
+++ Assets/Jonas/Scripts/MessageController.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MessageController : MonoBehaviour
+{
+    private List<GameObject> messages;
+    private int messagePos = 0;
+
+    private void Start()
+    {
+        messages = new List<GameObject>();
+
+        foreach (Transform t in transform)
+        {
+            messages.Add(t.gameObject);
+            t.gameObject.SetActive(false);
+        }
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            DisplayNextMessage();
+        }
+    }
+
+    private void DisplayNextMessage()
+    {
+        if(messagePos >= messages.Count)
+        {
+            MessagesDone();
+            return;
+        }
+
+        messages[messagePos++].SetActive(true);
+    }
+
+    private void MessagesDone()
+    {
+        Debug.Log("All messages done");
+    }
+}
